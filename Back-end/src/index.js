dce0f194ba839e9f46d0fc2470bae1980000cef6
Rysgothal <souzafrade2014@gmail.com>
2024-05-25 10:38:27 -0400

    Alterando IP para Deploy

--- Back-end/src/index.js
+++ Back-end/src/index.js
@@ -19,6 +19,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, async () => {
-    console.log(`API online => http://52.15.70.23:${port}`);
+    console.log(`API online => http://18.219.160.242:${port}`);
     await database().init();
 });
