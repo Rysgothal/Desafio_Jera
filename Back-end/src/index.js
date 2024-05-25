9c5f16864796eeedcdd7472ba0c47984a1d8d1d2
Rysgothal <souzafrade2014@gmail.com>
2024-05-24 23:49:43 -0400

    Testando certificado

--- Back-end/src/index.js
+++ Back-end/src/index.js
@@ -1,8 +1,11 @@
 
-const express = require('express');
 const { movieRouter } = require('./routers/movieRouter.js'); 
 const { database } = require('./database/database.js');
+const express = require('express');
 const cors = require('cors');
+
+const fs = require('fs');
+const https = require('https');
 
 const app = express();
 const port = 3050;
@@ -21,4 +24,11 @@ app.get('/', (req, res) => {
 app.listen(port, async () => {
     console.log(`API online => http://52.15.70.23:${port}`);
     await database().init();
+});
+
+https.createServer({
+    cert: fs.readFileSync('./src/SSL/code.crt'),
+    key: fs.readFileSync('./src/SSL/code.key') 
+}, app).listen(3001, () => {
+    console.log(`API online => https://localhost:${port}`);
 });
